366cfd91c213785a4baccfcd54e7c80b365576f4
Thomy Ledesma <thoomiledesma@gmail.com>
2023-10-19 23:29:40 -0300

    creada clase Curso
    instanciadas las materias y agregadas al diccionario, hay que modificar función ver_cursos() acorde

--- TUP-PII-TPI-II/curso.py
+++ TUP-PII-TPI-II/curso.py
@@ -24,10 +24,15 @@ materia = Curso("ingles")
 
 print(materia)
     
-        
+materia_1 = Curso("Ingles I")
+materia_2 = Curso("Ingles II")
+materia_3 = Curso("Laboratorio I")
+materia_4 = Curso("Laboratorio II")
+materia_5 = Curso("Programación I")
+materia_6 = Curso("Programación II")
 
 cursos = {
-    "Tecnicatura Universitaria en Programación" : ["Ingles II","Ingles I","Laboratiorio I","Laboratiorio II","Programación I","Programación II"],
+    "Tecnicatura Universitaria en Programación" : [materia_1,materia_2,materia_3,materia_4,materia_5,materia_6],
     "Ingenieria en sistemas" : ["Proximamente... ;)"]
 }
 
